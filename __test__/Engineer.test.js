48342d046b21164acdd0c914b1766164171d3de3
sidonie <shelliednnguyen@gmail.com>
2021-02-19 11:26:27 -0800

    engineer obect

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require( "../lib/Engineer" );
+
+test( "Set engineer's Github username.", () => {
+   const testGithutUsername = "shellienguyen";
+   const engineer = new Engineer( "Shellie", 38475, "Shellie@Shellie.com", testGithutUsername );
+
+   expect( engineer.githubUsername ).toBe( testGithutUsername );
+});
+
+test( "Get Github account using getGithub().", () => {
+   const testGithubUsername = "shellienguyen";
+   const engineer = new Engineer( "Shellie", 39482, "Shellie@Shellie.Com", testGithubUsername );
+
+   expect( engineer.getGithubUsername()).toBe( testGithubUsername );
+});
+
+test( "Use getRole() to return \"Engineer\" as role.", () => {
+   const testRole = "Engineer";
+   const engineer = new Engineer( "Shellie", 38472, "Shellie@Shellie.Com" );
+
+   expect( engineer.getRole( testRole )).toBe( testRole );
+});
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -3,14 +3,14 @@ const Employee = require( "../lib/Employee" );
 
 test( "Setting manager's office number.", () => {
    const testOfficeNumber = 888;
-   const employee = new Manager( "Shellie", 38472, "Shellie@Shellie.Com", testOfficeNumber );
+   const manager = new Manager( "Shellie", 38472, "Shellie@Shellie.Com", testOfficeNumber );
 
-   expect( employee.officeNumber ).toBe( testOfficeNumber );
+   expect( manager.officeNumber ).toBe( testOfficeNumber );
 });
 
 test( "Use getRole() to return \"Manager\" as role.", () => {
    const testRole = "Manager";
-   const employee = new Manager( "Shellie", 38472, "Shellie@Shellie.Com", 888 );
+   const manager = new Manager( "Shellie", 38472, "Shellie@Shellie.Com", 888 );
 
-   expect( employee.getRole( testRole )).toBe( testRole );
+   expect( manager.getRole( testRole )).toBe( testRole );
 });
