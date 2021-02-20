c4359fbfdbbaef39220f9ef6090122acd7aaeeb0
sidonie <shelliednnguyen@gmail.com>
2021-02-20 13:18:50 -0800

    background image

--- Index.js
+++ Index.js
@@ -2,7 +2,6 @@
 const fs = require( "fs" );
 const inquirer = require( "inquirer" );
 
-//const Employee = require( "./lib/Employee" );
 const Manager = require( "./lib/Manager" );
 const Engineer = require( "./lib/Engineer" );
 const Intern = require( "./lib/Intern" );
